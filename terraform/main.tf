aec540ec38267a1745c8102d5c7061306e5852c2
Justin Reagor <justin.reagor@joyent.com>
2017-07-10 09:26:26 -0700

    Check in CNS stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,49 +1,50 @@
 provider "triton" {}
 
 variable "image_id" {
-	type = "string"
-	default = "c2808f17-51c8-400e-a1bf-c89083988399"	
+    type = "string"
+    default = "37486be0-9e6e-444d-b71d-69fa3088a053"
 }
 
 variable "machine_count" {
-	default = 2
+    default = 2
 }
 
-variable "domain" {
-	type = "string"
-}
+# variable "domain" {
+#     type = "string"
+# }
 
-variable "subdomain" {
-	type = "string"
-}
+# variable "subdomain" {
+#     type = "string"
+# }
 
 resource "triton_machine" "app" {
-	count = "${var.machine_count}" 
+    count = "${var.machine_count}"
 
-	name = "hello-world-app-${count.index}"
-	package = "g4-highcpu-512M"
-	image = "${var.image_id}"
+    name = "hello-world-app-${count.index}"
+    package = "g4-highcpu-512M"
+    image = "${var.image_id}"
 
-	tags {
-		Role = "www"
-		Application = "Hello World"
-		Version = "1.0"
-	}
+    tags {
+        Role = "www"
+        Application = "Hello World"
+        Version = "1.0"
+        triton.cns.services = "hello-world"
+    }
 }
 
-resource "dnsimple_record" "app" {
-	count = "${var.machine_count}"
+# resource "dnsimple_record" "app" {
+#     count = "${var.machine_count}"
 
-	domain = "${var.domain}"	
-	name = "${var.subdomain}"
-	
-	type = "A"
-	ttl = "3600"
+#     domain = "${var.domain}"
+#     name = "${var.subdomain}"
 
-	value = "${triton_machine.app.*.primaryip[count.index]}"
-}
+#     type = "A"
+#     ttl = "3600"
+
+#     value = "${triton_machine.app.*.primaryip[count.index]}"
+# }
 
 resource "triton_firewall_rule" "www" {
-	rule = "FROM any TO tag Role = www ALLOW tcp PORT 80"
-	enabled = true
+    rule = "FROM any TO tag Role = www ALLOW tcp PORT 80"
+    enabled = true
 }
