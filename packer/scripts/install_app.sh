68c66dac71e6ac05e59be53190aed4c64254a12d
Justin Reagor <justin.reagor@joyent.com>
2017-07-10 10:27:19 -0700

    Remove a bunch of tabs that were found in some other version

--- packer/scripts/install_app.sh
+++ packer/scripts/install_app.sh
@@ -14,15 +14,15 @@ apt-get install -y tomcat8 nginx
 # Edit Tomcat configuratino to bind to loopback. For brevity, this
 # configuration is not packaged, but should be.
 sed -i \
-	'/<Connector port="8080"/a \t\t\t\taddress="127.0.0.1" ' \
-	/etc/tomcat8/server.xml
+    '/<Connector port="8080"/a address="127.0.0.1" ' \
+    /etc/tomcat8/server.xml
 
 # Copy nginx configuration into place. For brevity, this configuration is
 # not packaged, but should be.
 mv /tmp/tomcat /etc/nginx/sites-available/tomcat
 ln -s /etc/nginx/sites-available/tomcat /etc/nginx/sites-enabled/tomcat
 
-# Install our package. For simplicity we copy the package in rather 
+# Install our package. For simplicity we copy the package in rather
 # than configuring an APT repository, though this is straightforward
 # to host on Manta once it is created using Package Cloud, Aptly or
 # similar tools.
